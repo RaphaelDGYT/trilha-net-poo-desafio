eea6c49f285283b7396683dab56fe41675d6ef74
Raphael Dantas <130525602+RaphaelDGYT@users.noreply.github.com>
2025-06-03 20:56:18 +0000

    feat: Desafio concluído com sucesso (DIO)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("");
+
+Console.WriteLine("Testando iPhone:");
+Smartphone iphone = new Iphone("123456", "Modelo 1", "111111111", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
